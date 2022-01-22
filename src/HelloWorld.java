7314cdb6e2552761dc74758e6cdaaa7ec7a90f85
saifymatteo <muhdsaifulmashuri@gmail.com>
2022-01-22 11:41:16 +0800

    new commit with VS Code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {
+    public static void main(String[] args) {
+        int num = 2 + 2 + 3 + 4;
+        System.out.println(num);
+        var num1 = "2 + 2 " + 3 + 4;
+        System.out.println(num1);
+    }
 }
